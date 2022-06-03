de2a43a5887c35a5a2bf874975a781c7653bcbfb
Ruben Samaey <ruben.samaey@gmail.com>
2022-06-03 13:55:04 +0200

    updated gitignore to ignore zip files update notebook to use centroid function only once and get both X and Y out in a single call update of api to use a prediction class that loads the model on instantiation (not load model for each prediction)

--- api_container/main.py
+++ api_container/main.py
@@ -1,17 +1,16 @@
 from flask import Flask, request, Response
 import json
 import numpy as np
-
+#we are importing our prediction class from the prediction.py file
+from prediction import CarCostEstimator
 
 app=Flask(__name__)
 
-#we are importing our prediction function from the prediction.py file
-from prediction import car_price_prediction
+# instantiate the CarCostEstimator to load the model at Flask start-up
+app.CarPricePredictor = CarCostEstimator("../lgbr_cars.model")
 
-# all parameters needed for a prediction
-dict_keys = ('vehicletype','gearbox','powerps','model','km','regmonth','fueltype','brandid')
-
-general_error_msg = ('''Input to estimate should be provided as json to parameter 'prediction_input'. ''' +
+general_error_msg = ('''Input for prediction should be provided as json to urlparameter 'prediction_input'. ''' +
+                    ''' Note that all inputs are integer numbers.''' +
                     'Example of expected json:' +
                     ''' '{'vehicletype':'-1','gearbox':'1','powerps':'0','model':'118','km':'150000','regmonth':'0', ''' +
                     ''' 'fueltype':'1','brandid':'38'} ''' +
@@ -22,26 +21,30 @@ def validate_and_format_input(input_to_validate) -> dict:
         Return a dict with both 2D array of validated input and empty error,
         or empty validated error and an error message.
     """
+    model_parameters_tuple = app.CarPricePredictor.get_model_parameters()
+
     validated_and_formated_input = []
     error = ''
     # try to convert the serialized json back to json
-    try:
-        input_dict = json.loads(input_to_validate)
-    except Exception as e:
-        error = str(e)
-
-    # validate the provided json to have all the keys
-    validated_input_list = []
-    for key in dict_keys:
-        if input_dict.get(key):
-            validated_input_list.append(input_dict.get(key))    
-        else:
-            error= 'The ' + str(key)+ ' parameter is not provided. '
-            break
+    if (input_to_validate is None) or (not input_to_validate.strip('\"')):
+        error = 'There was no input provided. '
+    else:
+        try:
+            input_dict = json.loads(input_to_validate)
+        except Exception as e:
+            error = str(e)
+        # validate the provided json to have all the keys
+        validated_input_list = []
+        for key in model_parameters_tuple:
+            if input_dict.get(key):
+                validated_input_list.append(int(input_dict.get(key)))    
+            else:
+                error= 'The ' + str(key)+ ' parameter is not provided. '
+                break
     
-    # out of the validated list of parameters create a 2D array
-    validated_and_formated_input = np.array(validated_input_list)
-    validated_and_formated_input = validated_and_formated_input.reshape(1,-1)
+        # out of the validated list of parameters create a 2D array
+        validated_and_formated_input = np.array(validated_input_list)
+        validated_and_formated_input = validated_and_formated_input.reshape(1,-1)
 
     return {"validated_input":validated_and_formated_input, "error": error}
 
@@ -49,7 +52,11 @@ def validate_and_format_input(input_to_validate) -> dict:
 def index():
     # create empty response to be completed depending on outcome of prediction
     resp = Response()
-    if request.method=='GET':
+    predicted_value = 0.0
+    prediction_input = ''
+    response_data = {'error' : 'Did not seem to get any input!' + general_error_msg}
+    resp.status_code = 400
+    if request.method=='GET':           
         prediction_input = request.args.get('prediction_input')
         result_dict = validate_and_format_input(prediction_input)
         # if an error is found in the validation of the input then set response accordingly
@@ -60,7 +67,7 @@ def index():
         else:
             validated_prediction_input = result_dict.get('validated_input')
             try:
-                predicted_value = car_price_prediction(validated_prediction_input)
+                predicted_value = app.CarPricePredictor.car_price_prediction(validated_prediction_input)
             except Exception as e:
                 response_data = {'error' : str(e) + general_error_msg}
                 resp.status_code = 400
--- api_container/prediction.py
+++ api_container/prediction.py
@@ -2,25 +2,38 @@ import joblib
 import lightgbm as lgb
 import numpy as np
 
-def retrieve_model(path):
-    try:
-        file_handle = open(path,'rb')
-    except IOError as e:
-        raise(e)
-    trained_model = joblib.load(file_handle)
-    return trained_model
+class CarCostEstimator():
+    ''' Class implementing the prediction of a car price based on the input parameters given.
 
-def car_price_prediction(single_input) -> float:
-    lgbr_cars = retrieve_model("../lgbr_cars.model")
-    predicted_value = lgbr_cars.predict(single_input)
 
-    # predicted_value is of type numpy.ndarray with a single element
-    # get the float out
-    predicted_value_float = round(predicted_value.item(),3)
-    return predicted_value_float
+    Class will load the trained model on initialisation.
+    '''
+    model_parameters_tuple = ('vehicletype','gearbox','powerps','model','km','regmonth','fueltype','brandid')
+
+   
+    lgbr_cars = lgb.LGBMModel()
+
+    def __init__(self, path: str) -> None:
+        try:
+            file_handle = open(path,'rb')
+        except IOError as e:
+            raise(e)
+        print('created new CarCostEstimator with loaded model')
+        self.lgbr_cars = joblib.load(file_handle)
+
+    def get_model_parameters(self) -> tuple:
+        return self.model_parameters_tuple
+
+    def car_price_prediction(self, single_input) -> float:
+        predicted_value = self.lgbr_cars.predict(single_input)
+        # predicted_value is of type numpy.ndarray with a single element
+        # get the float out
+        predicted_value_float = round(float(predicted_value.item()),3)
+        return predicted_value_float
 
 # allow testing of the module independently
 if __name__ == '__main__':
+    CarCostPredictor = CarCostEstimator("../lgbr_cars.model")
     model_test_input = [[3,1,190,-1,125000,5,3,1]]
-    predicted_value = car_price_prediction(model_test_input)
+    predicted_value = CarCostPredictor.car_price_prediction(model_test_input)
     print(predicted_value)
